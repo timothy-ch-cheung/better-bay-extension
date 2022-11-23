ca2d33b3e598f84564722c5216b12538b779602a
Timothy Cheung <timothycheung@pop-os.localdomain>
2022-11-23 00:06:00 +0000

    Use plasmo storage api to persist enabled setting

--- src/popup.tsx
+++ src/popup.tsx
@@ -1,9 +1,16 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import "../styles.css"
 import Toggle from "./components/toggle"
+import { useStorage } from "@plasmohq/storage/hook"
 
 function IndexPopup() {
-  const [betterBayEnabld, set] = useState("")
+  const [betterBayEnabled, setBetterBayEnabled] = useStorage("hailing", async (v) => v === undefined ? false : v)
+
+
+  const handleOnClick = () => {
+    console.log(!betterBayEnabled)
+    setBetterBayEnabled(!betterBayEnabled)
+  }
 
   return (
     <div
@@ -17,7 +24,7 @@ function IndexPopup() {
         <h1 className="text-center font-bold text-base">Better Bay</h1>
       </div>
       <hr className="my-2" />
-      <Toggle label="Enabled" enabled={true} />
+      <Toggle label="Enabled" enabled={betterBayEnabled} onClick={handleOnClick} />
       <hr className="my-2" />
       <a href="https://github.com/timothy-ch-cheung/better-bay-extension" target="_blank">
         View Docs
